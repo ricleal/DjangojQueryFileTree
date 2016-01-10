eef2019a85f45be31fb8852c5aa0d5c3757ed068
Ricardo Ferraz Leal <ricleal@gmail.com>
2016-01-10 10:32:08 -0500

    Added base.html

--- proj/app1/models.py
+++ proj/app1/models.py
@@ -2,4 +2,6 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+# class Data(models.Model):
+#     file_data = models.CharField(max_length=100)
+#     file_bckg = models.CharField(max_length=100)
